7e6dcbf7093d5927b3b5880906100703f26a85a7
Ahmed Ibrahim <ahmedibrahem22322@gmail.com>
2021-04-06 20:18:44 +0200

    after adding rds & elsticashe

--- elsticash.tf
+++ elsticash.tf
@@ -3,7 +3,9 @@ resource "aws_elasticache_cluster" "els-cashe" {
   engine               = "redis"
   node_type            = "cache.t2.micro"
   engine_version       = "3.2.10"
+  num_cache_nodes      = 1
   port                 = 6379
+  parameter_group_name = "default.redis3.2"
   subnet_group_name = aws_elasticache_subnet_group.bar.name
   security_group_ids = [aws_security_group.redis.id]
 }
