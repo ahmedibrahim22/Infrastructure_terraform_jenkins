9c2316314184fe7ae20bab362a0eccb7ea6cf888
Ahmed Ibrahim <ahmedibrahem22322@gmail.com>
2021-04-06 20:36:54 +0200

    after adding rds & elsticashe

--- rds.tf
+++ rds.tf
@@ -1,9 +1,12 @@
 resource "aws_db_instance" "default" {
+  allocated_storage    = 10
   engine               = "mysql"
   engine_version       = "5.7"
   instance_class       = "db.t2.micro"
   name                 = "mydb"
   parameter_group_name = "default.mysql5.7"
+  username             = "admin"
+  password             = "admin123"
   port                 = "3306"
   vpc_security_group_ids = [aws_security_group.rds.id]
   db_subnet_group_name = aws_db_subnet_group.my_db_rds.name 
